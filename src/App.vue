2bf343b5ab38bd683de481349bacaecd94556b7a
Jinnie Cho <jinniethecho@gmail.com>
2021-08-13 16:16:10 +0900

    🎉 Created vuelog project's boilerplate

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+   
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica Neue, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  box-sizing: border-box;
+  margin: 0; 
+}
+</style>
