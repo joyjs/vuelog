0dc60adb52e75b5c9f52fe0687010ed8b9bdfca1
Jinnie Cho <yejincho1105@gmail.com>
2021-08-18 21:24:32 +0900

    🚧 Working on errors

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,13 @@ module.exports = {
     css: {
         loaderOptions: { 
             sass: {
-                // additionalData: `@import "./src/assets/_shared.scss";`
+                // additionalData: `@import "./src/_shared.scss";`,
+                additionalData: `@import "./src/styles/constants/_typography.scss";`,
                 additionalData: `@import "./src/styles/base/_normalize.scss";`,
                 additionalData: `@import "./src/styles/base/_reset.scss";`,
                 additionalData: `@import "./src/styles/base/_globals.scss";`,
                 additionalData: `@import "./src/styles/layouts/_grids.scss";`,
                 additionalData: `@import "./src/styles/constants/_colors.scss";`,
-                // additionalData: `@import "./src/styles/constants/_typography.scss";`,
                 additionalData: `@import "./src/styles/mixins/_flexbox.scss";`,
                 additionalData: `@import "./src/styles/mixins/_text-style.scss";`,
               },
