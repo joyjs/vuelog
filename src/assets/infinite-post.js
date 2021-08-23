6ee4310594025f767f8b21953784b4d297208f67
Jinnie Cho <yejincho1105@gmail.com>
2021-08-23 16:35:36 +0900

    🍱 Added cards data

---
+++ src/assets/infinite-post.js
@@ -0,0 +1,68 @@
+export default [
+  {
+    id: 0,
+    title: 'The New Normal',
+    subtitle: 'How to build a Design education program for creative work',
+    image:
+      'https://airbnb.design/wp-content/uploads/2020/11/Service-Design-Header-3.png',
+    categories: ['Behind the scens', 'Case Study']
+  },
+  {
+    id: 1,
+    title: 'Immerging Tyrus',
+    subtitle: 'Our free digital toolkit for freelance illustrators',
+    image:
+      'https://airbnb.design/wp-content/uploads/2020/10/Tyrus-Header-for-Airbnb.Design-1.jpg',
+    categories: ['News', 'Projects']
+  },
+  {
+    id: 2,
+    title: 'Introducing Quicksand',
+    subtitle: 'Explore our new film series on overcoming creative adversity.',
+    image: 'https://airbnb.design/wp-content/uploads/2020/09/Header-Final.png',
+    categories: ['Inspirations', 'Interview', 'Project']
+  },
+  {
+    id: 3,
+    title: 'Closing the Experience Gap ',
+    subtitle: '3 ways to use research to build more inclusive products',
+    image:
+      'https://airbnb.design/wp-content/uploads/2020/08/wordpress-header-3x.jpg',
+    categories: ['Case Study']
+  }
+  //   {
+  //     id: 4,
+  //     title: '',
+  //     subtitle: '',
+  //     image: '',
+  //     categories: ['']
+  //   }
+  // {
+  //     id: 0,
+  //     title: '',
+  //     subtitle: '',
+  //     image: '',
+  //     categories: [''],
+  // },
+  // {
+  //     id: 0,
+  //     title: '',
+  //     subtitle: '',
+  //     image: '',
+  //     categories: [''],
+  // },
+  // {
+  //     id: 0,
+  //     title: '',
+  //     subtitle: '',
+  //     image: '',
+  //     categories: [''],
+  // },
+  // {
+  //     id: 0,
+  //     title: '',
+  //     subtitle: '',
+  //     image: '',
+  //     categories: [''],
+  // },
+]
---
+++ src/assets/recent-post.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 0,
+    title: 'Learning Lessons',
+    subtitle: 'How to build a Design education program for creative work',
+    image:
+      'https://airbnb.design/wp-content/uploads/2021/06/WebHeaderLarge.png',
+    categories: ['Behind the scens', 'Perspectives']
+  },
+  {
+    id: 1,
+    title: 'Archetypes for creative leadership',
+    subtitle: 'How to define the behaviors you value',
+    image:
+      'https://airbnb.design/wp-content/uploads/2021/06/Leadership-archetypes-blog-header.png',
+    categories: ['Behind the scens', 'Perspectives']
+  },
+  {
+    id: 2,
+    title: 'Designing for crisis',
+    subtitle: '5 learnings from developing trauma-informed products',
+    image:
+      'https://airbnb.design/wp-content/uploads/2021/01/Header_Trauma-Informed-Design.jpg',
+    categories: ['Behind the scens', 'Case Study']
+  }
+]
