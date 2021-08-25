e7142d6fe5a37a07138361c6c513668dc12c0297
Jinnie Cho <yejincho1105@gmail.com>
2021-08-25 17:30:52 +0900

    🎨 Improved structures

--- src/components/RecentPost.vue
+++ src/components/RecentPost.vue
@@ -1,90 +1,88 @@
 <template>
-  <section class="recent-posts">
-    <div class="recent-post-1">
-      <div class="recent-post-thumblink">
-        <img
-          src="https://airbnb.design/wp-content/uploads/2021/06/WebHeaderLarge.png"
-          alt="Recent post image"
-        />
-      </div>
-      <div class="recent-post-texts">
-        <div class="recent-post-title-group">
-          <h1 class="recent-post-title">
-            Learning Lessons
-          </h1>
-          <h1 class="recent-post-subtitle">
-            How to build a Design education program for creative work
-          </h1>
-        </div>
-        <div class="recent-post-categories">
-          <div class="visually-hidden">
-            Categories
+  <section class="recent-posts container">
+    <div class="row">
+      <div class="column">
+        <article class="recent-post-1">
+          <a class="recent-post-thumblink" />
+          <div class="recent-post-texts">
+            <a class="recent-post-title-group">
+              <h1 class="recent-post-title">
+                Learning Lessons
+              </h1>
+              <h1 class="recent-post-subtitle">
+                How to build a Design education program for creative work
+              </h1>
+            </a>
+            <div class="recent-post-categories">
+              <div class="visually-hidden">
+                Categories
+              </div>
+              <a href="#">
+                Behind the scenes
+              </a>
+              <a href="#">
+                Perspectives
+              </a>
+            </div>
           </div>
-          <a href="#">
-            Behind the scenes
-          </a>
-          <a href="#">
-            Perspectives
-          </a>
-        </div>
+        </article>
       </div>
     </div>
-    <div class="recent-post-2">
-      <div class="recent-post-thumblink">
-        <img
-          src="https://airbnb.design/wp-content/uploads/2021/06/Leadership-archetypes-blog-header.png"
-          alt="Recent post image"
-        />
-      </div>
-      <div class="recent-post-texts">
-        <div class="recent-post-title-group">
-          <h1 class="recent-post-title">
-            Archetypes for creative leadership
-          </h1>
-          <h1 class="recent-post-subtitle">
-            How to define the behaviors you value
-          </h1>
-        </div>
-        <div class="recent-post-categories">
-          <div class="visually-hidden">
-            Categories
+
+    <div class="row sub-posts">
+      <div class="column">
+        <article class="recent-post-2">
+          <a class="recent-post-thumblink" />
+
+          <div class="recent-post-texts">
+            <a class="recent-post-title-group">
+              <h1 class="recent-post-title">
+                Archetypes for creative leadership
+              </h1>
+              <h1 class="recent-post-subtitle">
+                How to define the behaviors you value
+              </h1>
+            </a>
+            <div class="recent-post-categories">
+              <div class="visually-hidden">
+                Categories
+              </div>
+              <a href="#">
+                Behind the scenes
+              </a>
+              <a href="#">
+                Perspectives
+              </a>
+            </div>
           </div>
-          <a href="#">
-            Behind the scenes
-          </a>
-          <a href="#">
-            Perspectives
-          </a>
-        </div>
+        </article>
       </div>
-    </div>
-    <div class="recent-post-3">
-      <div class="recent-post-thumblink">
-        <img
-          src="https://airbnb.design/wp-content/uploads/2021/01/Header_Trauma-Informed-Design.jpg"
-          alt="Recent post image"
-        />
-      </div>
-      <div class="recent-post-texts">
-        <div class="recent-post-title-group">
-          <h1 class="recent-post-title">
-            Designing for crisis
-          </h1>
-          <h1 class="recent-post-subtitle">
-            5 learnings from developing trauma-informed products
-          </h1>
-        </div>
-        <div class="recent-post-categories">
-          <div class="visually-hidden">
-            Categories
+      <div class="column">
+        <article class="recent-post-3">
+          <a class="recent-post-thumblink" />
+
+          <div class="recent-post-texts">
+            <a class="recent-post-title-group">
+              <h1 class="recent-post-title">
+                Designing for crisis
+              </h1>
+              <h1 class="recent-post-subtitle">
+                5 learnings from developing trauma-informed products
+              </h1>
+            </a>
+            <div class="recent-post-categories">
+              <div class="visually-hidden">
+                Categories
+              </div>
+              <a href="#">
+                Behind the scenes
+              </a>
+              <a href="#">
+                Case Study
+              </a>
+            </div>
           </div>
-          <a href="#">
-            Behind the scenes
-          </a>
-          <a href="#">
-            Case Study
-          </a>
-        </div>
+        </article>
       </div>
     </div>
   </section>
@@ -92,10 +90,7 @@
 
 <script>
 export default {
-  name: 'RecentPost',
-  props: {
-    recentPost: Object
-  }
+  name: 'RecentPost'
 }
 </script>
 
@@ -108,71 +103,154 @@ export default {
 @import '../styles/mixins/_text-style.scss';
 @import '../styles/mixins/_flexbox.scss';
 
-.recent-posts {
+.container {
   @include column-flexbox();
-  padding: 0 24px;
+}
+
+.row {
+  @include flexbox();
+  flex-wrap: wrap;
+  width: 100%;
+  background-color: #fff;
+}
+
+.column {
+  @include column-flexbox();
+  width: 100%;
+  flex-basis: 100%;
+  background-color: #fff;
 }
 
 .recent-post-1,
 .recent-post-2,
 .recent-post-3 {
-  @include column-flexbox();
-  margin-bottom: 48px;
   width: 100%;
+  height: auto;
+  margin-bottom: 48px;
   border: 1px solid $grey-2b;
-}
-
-.recent-post-1 img {
-  width: auto;
-  height: 30vh;
-}
-
-.recent-post-2 img {
-  width: auto;
-  height: 30vh;
-}
-
-.recent-post-3 img {
-  width: auto;
-  height: 30vh;
 }
 
 .recent-post-thumblink {
   @include flexbox();
   width: 100%;
-  margin-bottom: 32px;
+  height: 30vh;
   overflow: hidden;
+  margin-bottom: 32px;
+}
+
+.recent-post-1 .recent-post-thumblink {
+  background-image: url('https://airbnb.design/wp-content/uploads/2021/06/WebHeaderLarge.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.recent-post-2 .recent-post-thumblink {
+  background-image: url('https://airbnb.design/wp-content/uploads/2021/06/Leadership-archetypes-blog-header.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.recent-post-3 .recent-post-thumblink {
+  background-image: url('https://airbnb.design/wp-content/uploads/2021/01/Header_Trauma-Informed-Design.jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 
 .recent-post-texts {
-  padding: 0 8px;
-}
-
-.recent-post-title-group {
-  margin-bottom: 32px;
+  @include column-flexbox(between, start);
+  padding: 0 8px 24px;
+  min-height: 312px;
 }
 
 .recent-post-title {
   @include text-style(h1, $grey-2b);
   display: inline;
-  margin-right: 10px;
 }
 
 .recent-post-subtitle {
   @include text-style(h1-subtitle, $grey-76);
   display: inline;
-  margin: 0;
 }
 
 .recent-post-categories {
-  margin-bottom: 24px;
+  @include text-style(categories, $grey-4a);
+  margin-left: 0;
 }
 
 .recent-post-categories a::after {
   content: ',';
 }
 
-.recent-post-categories a:last-child:after {
+.recent-post-categories a:last-child::after {
   content: '';
+}
+
+@media screen and (min-width: 768px) {
+  .column {
+    flex: 1;
+  }
+
+  .recent-post-texts {
+    min-height: 0;
+  }
+
+  .recent-post-1 {
+    border: none;
+    margin-bottom: 64px;
+  }
+
+  .recent-post-1 .recent-post-thumblink {
+    height: 592px;
+    overflow: visible;
+    margin-bottom: 0;
+  }
+
+  .recent-post-1 .recent-post-title-group {
+    margin-bottom: 32px;
+  }
+
+  .recent-post-1 .recent-post-texts {
+    padding: 24px 4px;
+    width: 50%;
+    transform: translateY(-73px);
+    border: 1px solid $grey-2b;
+    background-color: #fff;
+  }
+
+  .sub-posts {
+    gap: 64px;
+  }
+
+  .recent-post-2,
+  .recent-post-3 {
+    width: 100%;
+    height: auto;
+  }
+
+  .recent-post-2 .recent-post-thumblink {
+    max-height: 420px;
+    width: 100%;
+  }
+
+  .recent-post-2 .recent-post-texts {
+    min-height: 152px;
+  }
+
+  .recent-post-2 .recent-post-title-group {
+    margin-bottom: 32px;
+  }
+
+  .recent-post-3 .recent-post-thumblink {
+    max-height: 420px;
+    width: 100%;
+  }
+
+  .recent-post-3 .recent-post-texts {
+    min-height: 152px;
+  }
+  .recent-post-3 .recent-post-title-group {
+    margin-bottom: 32px;
+  }
 }
 </style>
