760accd3a6c10ca03ca45c6acb4d4a601e8467a6
Jinnie Cho <yejincho1105@gmail.com>
2021-08-26 01:18:00 +0900

    🍱  Updated post card assets

--- src/assets/infinite-post.js
+++ src/assets/infinite-post.js
@@ -5,7 +5,7 @@ export default [
     subtitle: 'How to build a Design education program for creative work',
     image:
       'https://airbnb.design/wp-content/uploads/2020/11/Service-Design-Header-3.png',
-    categories: ['Behind the scens', 'Case Study']
+    categories: ['Behind the scenes', 'Case Study']
   },
   {
     id: 1,
@@ -29,28 +29,30 @@ export default [
     image:
       'https://airbnb.design/wp-content/uploads/2020/08/wordpress-header-3x.jpg',
     categories: ['Case Study']
+  },
+  {
+    id: 4,
+    title: 'A Home Between Places',
+    subtitle: 'Opening doors to counter closed borders',
+    image: 'https://airbnb.design/wp-content/uploads/2017/01/tehran-v3.jpg',
+    categories: ['Behind the scens', 'Perspectives']
+  },
+  {
+    id: 5,
+    title: 'From the Ground Up',
+    subtitle:
+      'Our free digital toolkit for freelance illustrators Shining a light on how reserach guides our product development',
+    image:
+      'https://airbnb.design/wp-content/uploads/2016/08/DoorsCorners_Final-MS.png',
+    categories: ['Behind the scens']
+  },
+  {
+    id: 6,
+    title: 'Building a Visual Language',
+    subtitle: 'Behind the scenes of our new design system',
+    image: 'https://airbnb.design/wp-content/uploads/2016/05/dls-header-1.png',
+    categories: ['Behind the scens']
   }
-  //   {
-  //     id: 4,
-  //     title: '',
-  //     subtitle: '',
-  //     image: '',
-  //     categories: ['']
-  //   }
-  // {
-  //     id: 0,
-  //     title: '',
-  //     subtitle: '',
-  //     image: '',
-  //     categories: [''],
-  // },
-  // {
-  //     id: 0,
-  //     title: '',
-  //     subtitle: '',
-  //     image: '',
-  //     categories: [''],
-  // },
   // {
   //     id: 0,
   //     title: '',
