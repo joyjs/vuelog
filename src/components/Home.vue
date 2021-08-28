c99e3e9e06547b417afab764c78d216d8994bbb6
Jinnie Cho <yejincho1105@gmail.com>
2021-08-29 01:17:36 +0900

    ✨ Created home component

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <RecentPost />
+  <!-- <InfinitePost
+    :infinitePost="infinitePost[i]"
+    v-for="(a, i) in infinitePost"
+    :key="i"
+  /> -->
+</template>
+
+<script>
+import RecentPost from './RecentPost.vue'
+// import InfinitePost from './components/InfinitePost.vue'
+export default {
+  name: 'Home',
+  components: {
+    RecentPost
+    // InfinitePost
+  }
+}
+</script>
+
+<style></style>
