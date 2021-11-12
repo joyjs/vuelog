4f29f7db60bf8957186d745a4e8c90f0a596ded5
Jinnie Cho <yejincho1105@gmail.com>
2021-11-12 22:49:56 +0900

    🔨 Added publicPath script

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,19 @@
 module.exports = {
-    css: {
-        loaderOptions: { 
-            sass: {
-                // additionalData: `@import "./src/_shared.scss";`,
-                additionalData: `@import "./src/styles/constants/_typography.scss";`,
-                additionalData: `@import "./src/styles/base/_normalize.scss";`,
-                additionalData: `@import "./src/styles/base/_reset.scss";`,
-                additionalData: `@import "./src/styles/base/_globals.scss";`,
-                additionalData: `@import "./src/styles/layouts/_grids.scss";`,
-                additionalData: `@import "./src/styles/constants/_colors.scss";`,
-                additionalData: `@import "./src/styles/mixins/_flexbox.scss";`,
-                additionalData: `@import "./src/styles/mixins/_text-style.scss";`,
-              },
-        },
-    },
-};
+  publicPath: '/vuelog',
+
+  css: {
+    loaderOptions: {
+      sass: {
+        // additionalData: `@import "./src/assets/_shared.scss";`,
+        additionalData: `@import "./src/styles/constants/_typography.scss";`,
+        additionalData: `@import "./src/styles/base/_normalize.scss";`,
+        additionalData: `@import "./src/styles/base/_reset.scss";`,
+        additionalData: `@import "./src/styles/base/_globals.scss";`,
+        additionalData: `@import "./src/styles/layouts/_grids.scss";`,
+        additionalData: `@import "./src/styles/constants/_colors.scss";`,
+        additionalData: `@import "./src/styles/mixins/_flexbox.scss";`,
+        additionalData: `@import "./src/styles/mixins/_text-style.scss";`
+      }
+    }
+  }
+}
